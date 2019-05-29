a2cca7e4a43f1a86dc127ead45c9a2bce8c29903
sabas1080 <s@theinventorhouse.org>
2019-05-28 20:51:32 -0500

    Fix Line and Turn steps
    Thanks @ForeverWero

--- Makech/firmware/Escornabot/Configuration.h
+++ Makech/firmware/Escornabot/Configuration.h
@@ -111,8 +111,8 @@ See LICENSE.txt for details
 
 // step calibration
 #define STEPPERS_STEPS_PER_SECOND 1000
-#define STEPPERS_LINE_STEPS 1738
-#define STEPPERS_TURN_STEPS 1024
+#define STEPPERS_LINE_STEPS 1024
+#define STEPPERS_TURN_STEPS 1800
 
 #endif
 
